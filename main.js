12952cf67f6eb8f4eba36d616bf5e08a17523f5a
ProfThiagoScheibe <134386393+ProfThiagoScheibe@users.noreply.github.com>
2023-10-04 16:30:28 -0300

    Aula 4 - Final

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ document.querySelector("#som_tecla_vitoria").play()
 }
 
 document.querySelector(".tecla_vitoria").onclick = tocaSomVitoria;
+
+function tocaSomDinheiro(){
+    document.querySelector("#som_tecla_dinheiro").play()
+    }
+    
+    document.querySelector(".tecla_dinheiro").onclick = tocaSomDinheiro;
