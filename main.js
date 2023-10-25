0876caf8cd615d2530fc51af3e7d90c9cd2e440d
ProfThiagoScheibe <134386393+ProfThiagoScheibe@users.noreply.github.com>
2023-10-25 16:08:10 -0300

    tecla

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ let contador = 0;
 while(contador < listaDeTeclas.length){
 
     listaDeTeclas[contador].onclick = function() {
-        tocaSom(idElementoAudio);
+        tocaSom('#som_tecla_aplausos');
     }
     contador = contador + 1 ;
     console.log(contador) ;
