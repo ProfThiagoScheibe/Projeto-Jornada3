27bb5fbee5b4d5cba79a481859be5ff93547ec13
ProfThiagoScheibe <134386393+ProfThiagoScheibe@users.noreply.github.com>
2023-10-25 15:46:28 -0300

    listagem

--- main.js
+++ main.js
@@ -1,25 +1,3 @@
-function tocaSomVitoria(){
-document.querySelector("#som_tecla_vitoria").play()
-}
-
-document.querySelector(".tecla_vitoria").onclick = tocaSomVitoria;
-
-function tocaSomDinheiro(){
-    document.querySelector("#som_tecla_dinheiro").play()
-    }
-    
-    document.querySelector(".tecla_dinheiro").onclick = tocaSomDinheiro;
-
-function tocaSomVaia(){
-    document.querySelector("#som_tecla_vaia").play()
-}
-
-    document.querySelector(".tecla_vaia").onclick = tocaSomVaia;
-
-function tocaSomAplausos(){
-    document.querySelector('#som_tecla_aplausos').play()
-}
-
     const listaDeTeclas = document.querySelectorAll(".tecla");
 
 let contador = 0;
