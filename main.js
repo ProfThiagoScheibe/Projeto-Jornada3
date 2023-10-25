b75b3a597e49c7e4041e02a71f8980e4ad39d9fe
ProfThiagoScheibe <134386393+ProfThiagoScheibe@users.noreply.github.com>
2023-10-25 16:17:53 -0300

    Efeito e contadores

--- main.js
+++ main.js
@@ -2,11 +2,12 @@ function tocaSom(idElementoAudio) {
      document.querySelector(idElementoAudio).play(); 
 }
     const listaDeTeclas = document.querySelectorAll(".tecla");
-let contador = 0;
+    let contador = 0;
 while(contador < listaDeTeclas.length){
-
+    const efeito = listaDeTeclas[contador].classList[1]
+    const idAudio = "#son_" + efeito
     listaDeTeclas[contador].onclick = function() {
-        tocaSom('#som_tecla_aplausos');
+        tocaSom(idAudio);
     }
     contador = contador + 1 ;
     console.log(contador) ;
