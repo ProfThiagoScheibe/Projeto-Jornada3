cb3cab150bcda962b2906da0295e144b644c29fa
ProfThiagoScheibe <134386393+ProfThiagoScheibe@users.noreply.github.com>
2023-11-08 16:06:57 -0300

    Contador For

--- main.js
+++ main.js
@@ -2,13 +2,24 @@ function tocaSom(idElementoAudio) {
      document.querySelector(idElementoAudio).play() ; 
 }
     const listaDeTeclas = document.querySelectorAll(".tecla");
-    let contador = 0;
-while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_" + efeito ;
-    listaDeTeclas[contador].onclick = function() {
+//    
+//   let contador = 0;
+//while(contador < listaDeTeclas.length){
+//    const tecla = listaDeTeclas[contador]
+//    const efeito = tecla.classList[1];
+//    const idAudio = `#som_${efeito}` ;
+//    tecla.onclick = function() {
+//        tocaSom(idAudio);
+//   }
+//    contador = contador + 1 ;
+   // console.log(contador) ;
+}
+
+for(let contador = 0; contador < listaDeTeclas.length; contador = contador + 1) {
+    const tecla = listaDeTeclas[contador] ;
+    const efeito = tecla.classList[1] ;
+    const idAudio = `#som_${efeito}` ;
+    tecla.onclick = function() {
         tocaSom(idAudio);
     }
-    contador = contador + 1 ;
-    console.log(contador) ;
 }
